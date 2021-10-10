fca796d5efd2e557af550474672ae40505e577a5
Templars <antoine.ratat@gmail.com>
2021-10-11 05:52:22 +0800

    route get post todos get put delete todo by id

--- run.py
+++ run.py
@@ -66,6 +66,8 @@ class Todo(db.Model):
 
 # --- INFO: REACT FUNCTIONS --- 
 
+# CRUD USERS
+
 def getUsers():
     users = User.query.all()
     return jsonify(users=[user.serialize for user in users])
@@ -92,7 +94,6 @@ def getUser(user_id):
 
 def updateUser(user_id, email, password, first_name, last_name):
     user = User.query.get(user_id)
-    print(user_id)
     if not user:
         return jsonify({"message": 'User doesn\'t exist'}), 404
     if email:
@@ -126,7 +127,63 @@ def deleteUser(user_id):
         return make_response(jsonify({"message": 'Removed user with ID: {}'.format(user_id)}))
     except:
         db.session.rollback()
-        return jsonify({"message": "Couldn't add user to DB"}), 400
+        return jsonify({"message": "Couldn't delete user to DB"}), 400
+
+# CRUD TODOS
+
+def getTodos():
+    todos = Todo.query.all()
+    return jsonify(totods=[todo.serialize for todo in todos])
+
+def postTodo(todo_description, completed, user_id):
+    user = User.query.get(user_id)
+    if not user:
+        return jsonify({'message': 'No user associated'}), 400
+    todo = Todo(todo_description=todo_description, completed=completed, user_id=user_id)
+    db.session.add(todo)
+    try:
+        db.session.commit()
+        return jsonify(todo=todo.serialize)
+    except:
+        db.session.rollback()
+        return jsonify({"message": "Couldn't add todo to DB"}), 400
+
+def getTodo(todo_id):
+    todo = Todo.query.get(todo_id)
+    if not todo:
+        return jsonify({"message": "Todo doesn\'t exist"}), 404
+    return jsonify(todo=todo.serialize)
+
+def updateTodo(todo_id, todo_description, completed, user_id):
+    user = User.query.get(user_id)
+    todo = Todo.query.get(todo_id)
+    if not user:
+        return jsonify({'message': 'No user associated'}), 400
+    if todo_description:
+        todo.todo_description = todo_description
+    if completed:
+        todo.completed = completed
+    if user_id:
+        todo.user_id = user_id
+    db.session.add(todo)
+    try:
+        db.session.commit()
+        return jsonify(user=user.serialize)
+    except:
+        db.session.rollback()
+        return jsonify({"message": "Couldn't add user to DB"})
+
+def deleteTodo(todo_id):
+    todo = Todo.query.get(todo_id)
+    if not todo:
+        return jsonify({"message": "Todo doesn't exist"}), 404
+    db.session.delete(todo)
+    try:
+        db.session.commit()
+        return make_response(jsonify({"message": 'Removed todo with ID: {}'.format(todo_id)}))
+    except:
+        db.session.rollback()
+        return jsonify({"message": "Couldn't delete todo to DB"}), 400
 
 # --- INFO: ROUTES ---
 
@@ -176,6 +233,46 @@ def adminUser(user_id):
 
     if request.method == 'DELETE':
         return deleteUser(user_id)
+
+@app.route('/api/todos', methods=['GET', 'POST'])
+def todos():
+    if request.method == 'GET':
+        return getTodos()
+    if request.method == 'POST':
+        if not request.is_json:
+            return jsonify({"message": "Missing JSON in request"}), 400
+        content = request.get_json(force=True)
+        todo_description = content.get("todo_description", None)
+        completed = content.get("completed", None)
+        user_id = content.get("user_id", None)
+        if not todo_description: 
+            return jsonify({"message": 'Missing todo_description in JSON'})
+        if not completed: 
+            return jsonify({"message": 'Missing completed in JSON'})
+        if not user_id: 
+            return jsonify({"message": 'Missing user_id in JSON'})
+        return postTodo(todo_description, completed, user_id)
+
+@app.route('/api/todo/<int:todo_id>', methods=['GET', 'PUT', 'DELETE']) 
+def todo(todo_id):
+    if not todo_id:
+        return jsonify({"message": "Missing todo_id in request"}), 404
+
+    if request.method == 'GET':
+        return getTodo(todo_id)
+
+    if request.method == 'PUT':
+        if not request.is_json:
+            return jsonify({"message": "Missing JSON in request"}), 400
+        content = request.get_json(force=True)
+        todo_description = content['todo_description'] if 'todo_description' in content.keys() else ''
+        completed = content['completed'] if 'completed' in content.keys() else ''
+        user_id = content['user_id'] if 'user_id' in content.keys() else ''
+        return updateUser(todo_id, todo_description, completed, user_id)
+
+    if request.method == 'DELETE':
+        return deleteTodo(todo_id)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
