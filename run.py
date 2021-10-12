376406de5ef31ea26b3affc33e9a23098d662532
Templars <antoine.ratat@gmail.com>
2021-10-12 16:41:11 +0800

    update from config.json to os.environ

--- run.py
+++ run.py
@@ -6,17 +6,21 @@ import datetime
 from flask_bcrypt import Bcrypt
 from flask_cors import CORS
 import json
+import os
 
-# # --- INFO: LOAD CONFIG VARIABLES ---
-with open('config.json') as config_file:
-    config = json.load(config_file)
+# with open('config.json') as config_file:
+#     config = json.load(config_file)
+
+# app.config['SQLALCHEMY_DATABASE_URI'] = config.get('DATABASE_URL')
+# app.config['SECRET_KEY'] =  config.get('SECRET_KEY')
+# app.config['JWT_SECRET_KEY'] = config.get('JWT_SECRET_KEY')
 
 # --- INFO: APP CONFIGURATION ---
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = config.get('DATABASE_URL')
-app.config['SECRET_KEY'] =  config.get('SECRET_KEY')
-app.config['JWT_SECRET_KEY'] = config.get('JWT_SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.config['JWT_SECRET_KEY'] = os.environ.get('JWT_SECRET_KEY')
 app.config['JWT_ACCESS_TOKEN_EXPIRES'] = datetime.timedelta(days=7)
 app.config['JWT_REFRESH_TOKEN_EXPIRES'] = datetime.timedelta(days=7)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
